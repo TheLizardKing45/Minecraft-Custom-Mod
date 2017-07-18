06f3c416151c28f3891961d78100b7554682cbe9
Lukas Olson <lukasdragon@live.ca>
2017-07-18 12:09:05 -0700

    Added the 4 tech ores
    4 tech ores and changed the creative tab icon

--- src/main/java/com/mrbengonio/first/util/RegisterUtil.java
+++ src/main/java/com/mrbengonio/first/util/RegisterUtil.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class RegisterUtil {
 	// register it here
 	public static void registerAll(FMLPreInitializationEvent event) {
-		registerBlocks(event, Blocks.LukasBlock);
+		registerBlocks(event, Blocks.cupaliteOre, Blocks.kreuthilOre, Blocks.saspumOre, Blocks.xasmoOre);
 		registerItems(event, Items.obsidianingot, Items.testItem);
 	}
 
