1cae052afb6c1e318496e0229cd636813f2c8273
Lukas Olson <lukasdragon@live.ca>
2017-07-30 21:17:34 -0700

    reverted previous commit

---
+++ src/main/java/com/mrbengonio/first/handlers/KeybindHandler.java
@@ -0,0 +1,28 @@
+package com.mrbengonio.first.handlers;
+
+import org.lwjgl.input.Keyboard;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+
+public class KeybindHandler {
+
+	private static KeyBinding[] keyBindings = new KeyBinding[1];
+
+	public static KeyBinding[] getKeyBindings() {
+		return keyBindings;
+	}
+
+	public static void RegisterKeyBindings() {
+		// declare an array of key bindings
+
+		// instantiate the key bindings
+		keyBindings[0] = new KeyBinding("key.armorutil.desc", Keyboard.KEY_G, "key.kreuthil.category");
+
+		// register all the key bindings
+		for (int i = 0; i < keyBindings.length; ++i) {
+			ClientRegistry.registerKeyBinding(keyBindings[i]);
+		}
+	}
+
+}
